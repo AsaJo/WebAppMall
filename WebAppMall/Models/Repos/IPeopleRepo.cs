86da5f6d2b5e94345c50a3c8726c57510591ee0d
Åsa Jonsson <Asa.Jonsson@lexicon.se>
2022-04-28 15:09:38 +0200

    Add project files.

---
+++ WebAppMall/Models/Repos/IPeopleRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMall.Models.Repos
+{
+    interface IPeopleRepo
+    {
+        //Interface with following methods.
+        // ▪ Person Create(Person person)
+        // ▪ List<Person> Read()
+        // ▪ Person Read(int id)
+        // ▪ bool Update(Person person)
+        // ▪ bool Delete(Person person)
+    }
+}
---
+++ WebAppMall/Models/Repos/InMemoryPeopleRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMall.Models.Repos
+{
+    public class InMemoryPeopleRepo
+    {
+        // Private static List of Person. 
+        // Private static int idCounter.
+    }
+}
